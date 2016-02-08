32eadbd1c836cf092fa4dc6c351ee2d8445f32a1
Alan Waidmann <alan.waidmann@gmail.com>
2016-02-08 13:58:34 -0800

    Configure React env

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Menu = require('./components/Menu')
+
+ReactDOM.render(
+  <Menu />,
+  document.getElementById('react')
+)
---
+++ js/components/Menu.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+class Menu extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>Hello World</div>
+  }
+}
+
+module.exports = Menu
